35fea6e3b4495bc279db582cecb6a1aac02b2e82
telepatehticman <telepatheticman13@gmail.com>
2020-10-31 18:33:28 -0600

    update about
    test another image

--- DesktopDokies/About.Designer.cs
+++ DesktopDokies/About.Designer.cs
@@ -51,9 +51,9 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(58, 34);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(354, 25);
+            this.label2.Size = new System.Drawing.Size(504, 25);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Alternate Doki Stickers by Ciiran";
+            this.label2.Text = "Alternate Doki Stickers and Clean Up by Ciiran";
             // 
             // label3
             // 
@@ -79,7 +79,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(525, 123);
+            this.ClientSize = new System.Drawing.Size(561, 123);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
